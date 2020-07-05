d8ed74ed6166e75e641b968044f82af57fb96188
Dele Fei <aaron040222@gmail.com>
2020-07-05 10:01:33 -0700

    more models

--- routes.py
+++ routes.py
@@ -11,10 +11,10 @@ def index():
 	features = ['BEDS', 'BATHS', 'SQFT', 'AGE', 'LOTSIZE', 'GARAGE']
 	prediction = model.predict([[4, 2.5, 3005, 15, 17903.0, 1]])[0][0].round(1)
 	prediction = str(prediction)
-	#prediction2 = model2.predict([[4, 2.5, 3005, 15, 17903.0, 1]])[0][0].round(1)
-	#prediction2 = str(prediction2)
-	#prediction3 = model3.predict([[4, 2.5, 3005, 15, 17903.0, 1]])[0][0].round(1)
-	#prediction3 = str(prediction3)
-	return render_template("index.html", prediction=prediction)
+	prediction2 = model2.predict([[4, 2.5, 3005, 15, 17903.0, 1]])[0][0].round(1)
+	prediction2 = str(prediction2)
+	prediction3 = model3.predict([[4, 2.5, 3005, 15, 17903.0, 1]])[0][0].round(1)
+	prediction3 = str(prediction3)
+	return render_template("index.html", prediction=prediction, prediction2=prediction2, prediction3=prediction3)
 
 
